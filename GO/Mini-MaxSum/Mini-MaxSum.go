d79b17baa8531cbba46721fb80e91f19e6225c4a
Lucas Lopes <lucasquin@protonmail.com>
2022-08-06 11:52:03 -0300

    MiniMax-Sum solution

---
+++ GO/Mini-MaxSum/Mini-MaxSum.go
@@ -0,0 +1,41 @@
+// Link do desafio: https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	// Declaro o tamanhdo do array e preencho ele com meu input
+	var arr [5]int
+	var min int
+	var max int
+
+	fmt.Scan(&arr[0], &arr[1], &arr[2], &arr[3], &arr[4])
+
+	// Ordeno o array em ordem crescente
+	sort.Slice(arr[:], func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	// Somo os 4 primeiros numeros menores
+	for i := 0; i < 4; i++ {
+		min = min + arr[i]
+	}
+
+	// Ordeno o array em ordem decrescente
+	sort.Slice(arr[:], func(i, j int) bool {
+		return arr[i] > arr[j]
+	})
+
+	// Somo os 4 primeiros numeros menores
+	for i := 0; i < 4; i++ {
+		max = max + arr[i]
+	}
+
+	// Imprimo
+	fmt.Println(min, max)
+}
